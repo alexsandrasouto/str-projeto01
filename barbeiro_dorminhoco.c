4ddfdaf590c84c932854de96e752cb4a6e1c0580
alexsandra.souto <alexsandra.souto@virtus.ufcg.edu.br>
2024-08-07 13:27:15 -0300

    Versão 3 - Após reunião com Kyller

--- barbeiro_dorminhoco.c
+++ barbeiro_dorminhoco.c
@@ -5,16 +5,16 @@
 #include <unistd.h>
 
 // Definições
-#define NUM_BARBEIROS 1
-#define NUM_CADEIRAS_ESPERA 1
-#define NUM_CLIENTES 5
+#define NUM_BARBEIROS 3
+#define NUM_CADEIRAS_ESPERA 5
+#define NUM_CLIENTES 20
 
 // Semáforos e mutex
 sem_t sem_clientes; // Conta o número de clientes esperando
 sem_t sem_barbeiros;
 sem_t sem_cadeiras_espera;
 
-pthread_mutex_t  mutex_cadeiras_espera, mutex_cadeira_barbeiro; // Exclusão mútua para as cadeiras de espera
+pthread_mutex_t  mutex_cadeiras_espera, mutex_cadeira_barbeiro[NUM_BARBEIROS]; // Exclusão mútua para as cadeiras de espera
 
 int barbeiros_ocupados[NUM_BARBEIROS] = {0};
 
@@ -28,7 +28,6 @@ void* cliente(void* id) {
 
         pthread_mutex_lock(&mutex_cadeiras_espera); 
         sem_getvalue(&sem_cadeiras_espera, &cadeiras_livres);
-        // printf("%d", cadeiras_livres);
 
         if(cadeiras_livres > 0){
 
@@ -36,44 +35,43 @@ void* cliente(void* id) {
 
             sem_getvalue(&sem_cadeiras_espera, &cadeiras_livres);
             printf("Cliente %d sentou na cadeira de espera. Cadeiras de espera livres: %d \n", cliente_id, cadeiras_livres);
-
             sem_post(&sem_clientes);
-            sem_wait(&sem_barbeiros);
             pthread_mutex_unlock(&mutex_cadeiras_espera);
 
-            pthread_mutex_lock(&mutex_cadeira_barbeiro);
+            sem_wait(&sem_barbeiros);
 
-            sem_getvalue(&sem_cadeiras_espera, &cadeiras_livres);
+            int barbeiro_id = 0;
+            int i;
+            for(i = 0; i < NUM_BARBEIROS; i++){
+                if (pthread_mutex_trylock(&mutex_cadeira_barbeiro[i]) == 0) {
+                    barbeiro_id = i+1;
 
-            for (int i = 0; i < NUM_BARBEIROS; i++) {
-                if (barbeiros_ocupados[i] == 0) {
-                    barbeiros_ocupados[i] = cliente_id;
-                    printf("Cliente %d está sendo atendido pelo barbeiro %d. Cadeiras de espera livres: %d \n", cliente_id, i + 1, cadeiras_livres);
-                    sem_post(&sem_cadeiras_espera);
-                    break;
+                    printf("Cliente %d está sendo atendido pelo barbeiro %d \n", cliente_id, barbeiro_id);
+                    sleep(3);
+
+                    printf("Cliente %d terminou o corte de cabelo e saiu. \n", cliente_id);
+        
+                    pthread_mutex_unlock(&mutex_cadeira_barbeiro[i]); 
+                    sem_post(&sem_barbeiros);
                 }
             }
 
-            pthread_mutex_unlock(&mutex_cadeira_barbeiro); // desbloqueia aqui mesmo??
+            // pthread_mutex_lock(&mutex_cadeira_barbeiro[barbeiro_id]);
 
-            sleep(3);
+            // printf("Cliente %d está sendo atendido pelo barbeiro %d \n", cliente_id, barbeiro_id);
+            // sleep(3);
 
-            printf("Cliente %d terminou o corte de cabelo e saiu. \n", cliente_id);
-
-            for (int i = 0; i < NUM_BARBEIROS; i++) {
-                if (barbeiros_ocupados[i] == cliente_id) {
-                    barbeiros_ocupados[i] = 0;
-                    break;
-                }   
-            }
-
-            sem_post(&sem_barbeiros);
+            // printf("Cliente %d terminou o corte de cabelo e saiu. \n", cliente_id);
+        
+            // pthread_mutex_unlock(&mutex_cadeira_barbeiro[barbeiro_id-1]); 
+            // sem_post(&sem_barbeiros);
 
             free(id);
             pthread_exit(NULL);
 
         }
         else{
+            pthread_mutex_unlock(&mutex_cadeiras_espera);
             printf("Cliente %d foi embora sem ser atendido. Não há cadeiras de espera livres. \n", cliente_id);
             free(id);
             pthread_exit(NULL);
@@ -90,6 +88,9 @@ void* barbeiro(void* id) {
 
     int barbeiro_id = *(int*)id;
     int clientes = 0;
+    int cadeiras_livres = 0;
+
+    // sem_post(&sem_barbeiros);
  
     while(1){
 
@@ -99,14 +100,19 @@ void* barbeiro(void* id) {
             printf("O barbeiro %d está dormindo. \n", barbeiro_id);
         }
 
-        sem_post(&sem_barbeiros);
         sem_wait(&sem_clientes);
 
-        printf("O barbeiro %d está acordado. Atentendo um cliente. \n", barbeiro_id);
+        pthread_mutex_lock(&mutex_cadeiras_espera);
+        sem_post(&sem_cadeiras_espera);
+        sem_getvalue(&sem_cadeiras_espera, &cadeiras_livres);
+
+        printf("O barbeiro %d está acordado. Atentendo um cliente. Cadeiras de espera livres: %d  \n", barbeiro_id, cadeiras_livres);
+        pthread_mutex_unlock(&mutex_cadeiras_espera);
+        
         sleep(3);
         printf("Barbeiro %d terminou de atender e está pronto para o próximo cliente.\n", barbeiro_id);
 
-        sem_post(&sem_barbeiros);
+        // sem_post(&sem_barbeiros);
     }
 
     free(id);
@@ -122,7 +128,7 @@ int main() {
     sem_init(&sem_cadeiras_espera, 0, NUM_CADEIRAS_ESPERA);
 
     pthread_mutex_init(&mutex_cadeiras_espera, NULL);
-    pthread_mutex_init(&mutex_cadeira_barbeiro, NULL);
+    pthread_mutex_init(&mutex_cadeira_barbeiro[NUM_BARBEIROS], NULL);
 
     pthread_t barbeiros[NUM_BARBEIROS];
     pthread_t clientes[NUM_CLIENTES];
@@ -150,7 +156,7 @@ int main() {
     sem_destroy(&sem_barbeiros);
     sem_destroy(&sem_cadeiras_espera);
     pthread_mutex_destroy(&mutex_cadeiras_espera);
-    pthread_mutex_destroy(&mutex_cadeira_barbeiro);
+    pthread_mutex_destroy(&mutex_cadeira_barbeiro[NUM_BARBEIROS]);
 
     return 0;
 }
